abea59e0d0039553a00196cb3278d31c4a4d0b00
abbaxi786 <141753795+abbaxi786@users.noreply.github.com>
2026-07-03 00:24:06 +0500

    The addition of first three tasks

---
+++ tasks/api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    class Status(models.TextChoices):
+        PENDING = 'PENDING', 'Pending'
+        IN_PROGRESS = 'IN_PROGRESS', 'In Progress'
+        COMPLETED = 'COMPLETED', 'Completed'
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
